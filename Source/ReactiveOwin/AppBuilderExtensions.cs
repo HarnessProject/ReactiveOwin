74dde6e375eb765cf9ed0eb90acea981350e5d7d
Nick Daniels <nick@fndsnd.com>
2014-04-28 06:48:17 -0400

    Initial Code Commit

---
+++ Source/ReactiveOwin/AppBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Owin;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace ReactiveOwin
+{
+    public static class AppBuilderExtensons
+    {
+        public static IObservable<OwinHandlerContext> AsObservable(this IAppBuilder app)
+        {
+            var r = new Subject<OwinHandlerContext>();
+            app.UseHandlerAsync(async (request, response, next) => await Task.Factory.StartNew(() => r.OnNext(new OwinHandlerContext { Request = request, Response = response, Next = next })));
+            return r;
+        }
+    }
+}
