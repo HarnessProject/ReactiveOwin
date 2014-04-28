abb5012fde6d79270a089838a47c44f57100f9b8
Nick Daniels <nick@fndsnd.com>
2014-04-28 06:49:25 -0400

    Whoops

---
+++ Source/ReactiveOwin/OwinHandlerContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin.Types;
+
+namespace ReactiveOwin
+{
+    public class OwinHandlerContext
+    {
+        public OwinRequest Request { get; set; }
+        public OwinResponse Response { get; set; }
+        public Func<Task> Next { get; set; }
+    }
+}
